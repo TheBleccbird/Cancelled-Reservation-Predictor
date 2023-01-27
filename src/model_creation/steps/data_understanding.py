62daa50df1a1e2dfa74b3087cde8078b1293eff6
Cristian Vacchiano <39271842+cris2499@users.noreply.github.com>
2023-01-27 18:59:09 +0100

    il metodo create_pie_chart in utils.py salva il grafico invece che mostrarlo Aggiunti nuovi metodi per creazione grafici Aggiunto nella classe data_understanding.py nuove rilevazioni anomalie Aggiunte creazioni grafici per la maggior parte delle feature

--- src/model_creation/steps/data_understanding.py
+++ src/model_creation/steps/data_understanding.py
@@ -20,48 +20,38 @@ def data_understanding():
 
     balanced_count = dataset["is_canceled"].value_counts()
     file.write("\nIl bilanciamento del dataset di partenza è: \n" + str(balanced_count) + "\n")
-    utils.create_pie_chart(dataset, "Bilanciamento del dataset di partenza")
+    utils.create_pie_chart(dataset, "Bilanciamento del dataset di partenza", "bilanciamento_iniziale")
 
     # individuazione di possibili outlier
     file.write("\nDistribuzione dati per la feature [lead_time]\n")
     file.write(str(dataset.lead_time.describe()) + '\n')
-    utils.detect_outliers(dataset, "lead_time")
 
     file.write("\nDistribuzione dati per la feature [stays_in_weekend_nights]\n")
     file.write(str(dataset.stays_in_weekend_nights.describe()) + '\n')
-    utils.detect_outliers(dataset, "stays_in_weekend_nights")
 
     file.write("\nDistribuzione dati per la feature [stays_in_week_nights]\n")
     file.write(str(dataset.stays_in_week_nights.describe()) + '\n')
-    utils.detect_outliers(dataset, "stays_in_week_nights")
 
     file.write("\nDistribuzione dati per la feature [adults]\n")
     file.write(str(dataset.adults.describe()) + '\n')
-    utils.detect_outliers(dataset, "adults")
 
     file.write("\nDistribuzione dati per la feature [children]\n")
     file.write(str(dataset.children.describe()) + '\n')
-    # utils.detect_outliers(dataset, "children")
 
     file.write("\nDistribuzione dati per la feature [babies]\n")
     file.write(str(dataset.babies.describe()) + '\n')
-    utils.detect_outliers(dataset, "babies")
 
     file.write("\nDistribuzione dati per la feature [previous_cancellations]\n")
     file.write(str(dataset.previous_cancellations.describe()) + '\n')
-    utils.detect_outliers(dataset, "previous_cancellations")
 
     file.write("\nDistribuzione dati per la feature [previous_bookings_not_canceled]\n")
     file.write(str(dataset.previous_bookings_not_canceled.describe()) + '\n')
-    utils.detect_outliers(dataset, "previous_bookings_not_canceled")
 
     file.write("\nDistribuzione dati per la feature [booking_changes]\n")
     file.write(str(dataset.booking_changes.describe()) + '\n')
-    utils.detect_outliers(dataset, "booking_changes")
 
     file.write("\nDistribuzione dati per la feature [days_in_waiting_list]\n")
     file.write(str(dataset.days_in_waiting_list.describe()) + '\n')
-    utils.detect_outliers(dataset, "days_in_waiting_list")
 
     file.write("\nDistribuzione dati per la feature [adr]\n")
     file.write(str(dataset.adr.describe()) + '\n')
@@ -69,20 +59,54 @@ def data_understanding():
 
     file.write("\nDistribuzione dati per la feature [total_of_special_requests]\n")
     file.write(str(dataset.total_of_special_requests.describe()) + '\n')
-    utils.detect_outliers(dataset, "total_of_special_requests")
 
+    file.write("\nAbbiamo notato che alcune prenotazioni non avevano clienti, ovvero 0 adulti, 0 bambini e 0 neonati. "
+               "Il numero di record che hanno quest'anomalia è: " + str(len(dataset[(dataset["adults"] == 0) &
+                                                                                    (dataset["children"] == 0) & (
+                                                                                                dataset[
+                                                                                                    "babies"] == 0)])))
+
+    file.write("\nAbbiamo notato che alcune prenotazioni avevano solo clienti non adulti, ovvero 0 adulti, almeno un "
+               "bambino e un neonato. "
+               "Il numero di record che hanno quest'anomalia è: " + str(len(dataset[(dataset["adults"] == 0) &
+                                                                                    (dataset["children"] >= 0) & (
+                                                                                            dataset[
+                                                                                                "babies"] >= 0)])))
     file.close()
 
+    # dati numerici
     utils.create_graph(dataset, "lead_time")
     utils.create_graph(dataset, "stays_in_weekend_nights")
     utils.create_graph(dataset, "stays_in_week_nights")
     utils.create_graph(dataset, "adults")
     utils.create_graph(dataset, "babies")
+    utils.create_graph(dataset, "children")
     utils.create_graph(dataset, "previous_cancellations")
     utils.create_graph(dataset, "previous_bookings_not_canceled")
     utils.create_graph(dataset, "booking_changes")
     utils.create_graph(dataset, "days_in_waiting_list")
     utils.create_graph(dataset, "adr")
     utils.create_graph(dataset, "total_of_special_requests")
+
+    # dati categorici
+    utils.create_count_plot(dataset, "is_canceled", "hotel")
+    utils.create_count_plot(dataset, "is_canceled", "arrival_date_year")
+    utils.create_count_plot(dataset, "is_canceled", "arrival_date_month")
+    utils.create_count_plot(dataset, "is_canceled", "arrival_date_week_number")
+    utils.create_count_plot(dataset, "is_canceled", "arrival_date_day_of_month")
+    utils.create_count_plot(dataset, "is_canceled", "meal")
+    # utils.create_count_plot(dataset, "is_canceled", "country")
+    utils.create_count_plot(dataset, "is_canceled", "market_segment")
+    utils.create_count_plot(dataset, "is_canceled", "distribution_channel")
+    utils.create_count_plot(dataset, "is_canceled", "reserved_room_type")
+    utils.create_count_plot(dataset, "is_canceled", "assigned_room_type")
+    utils.create_count_plot(dataset, "is_canceled", "deposit_type")
+    utils.create_count_plot(dataset, "is_canceled", "customer_type")
+    utils.create_count_plot(dataset, "is_canceled", "reservation_status")
+
+    utils.create_count_plot(dataset, "meal", "hotel")
+
+    # dati booleani
+    utils.create_count_plot(dataset, "is_canceled", "is_repeated_guest")
 
     return dataset
--- src/model_creation/steps/utils.py
+++ src/model_creation/steps/utils.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 
 
-def create_pie_chart(dataset, title):
+def create_pie_chart(dataset, title, file_title):
     count = dataset["is_canceled"].value_counts()
 
     data = [count[1], count[0]]
@@ -13,26 +13,23 @@ def create_pie_chart(dataset, title):
 
     plt.title(title)
     plt.show()
+    plt.savefig("src/images/" + file_title + ".png")
 
 
 def detect_outliers(dataset, column):
-    sns.boxplot(dataset[column])
-    plt.title(column)
-    plt.show()
-
     quartile_1 = dataset[column].quantile(0.25)
     quartile_3 = dataset[column].quantile(0.75)
     iqr = quartile_3 - quartile_1
     lower_bound = quartile_1 - (iqr * 1.5)
     upper_bound = quartile_3 + (iqr * 1.5)
-    print("Lower bound:", lower_bound)
-    print("Upper bound:", upper_bound)
-    outliers = dataset[(dataset[column] <= lower_bound) | (dataset[column] >= upper_bound)]
-    print("Numero di outlier: ", outliers.shape[0])
 
-    count = outliers.is_canceled.value_counts()
-    print("numero outlier prenotazioni confermate:", count[0])
-    print("numero outlier prenotazioni cancellate:", count[1])
+    return lower_bound, upper_bound
+
+
+def create_box_plot(dataset, column):
+    sns.boxplot(dataset[column])
+    plt.title(column)
+    plt.show()
 
 
 def create_graph(dataset, column):
@@ -43,7 +40,12 @@ def create_graph(dataset, column):
              color=['red', 'green'])
     plt.legend(loc='upper right')
 
-    plt.ylabel('numero istanze')
+    plt.ylabel('count')
     plt.xlabel(column)
     plt.title("Feature: " + column)
+    plt.show()
+
+
+def create_count_plot(dataset, x, y):
+    sns.countplot(dataset, x=x, hue=y)
     plt.show()
